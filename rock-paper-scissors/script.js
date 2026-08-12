d3f74a1712c5aaa21b77dfba2f6257b8cbe35538
Andreas <andrewtheodoses@gmail.com>
2026-08-12 17:00:54 +0300

    Implement getComputerChoice function

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    // returns a string: "rock", "paper", "scissors"
+    let num = Math.random();
+    if(num >= 0 && num <= 0.3333)
+        return "rock";
+    if(num > 0.3333 && num <= 0.6666)
+        return "paper";
+    if(num > 0.6666 && num < 1)
+        return "scissors";
+}
