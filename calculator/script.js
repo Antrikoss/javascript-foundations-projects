5e82965f4d76b8e56c308057daa0ea3c34edca65
Andreas <andrewtheodoses@gmail.com>
2026-08-19 20:06:00 +0300

    Implement 'operate' function to calculate operations

--- calculator/script.js
+++ calculator/script.js
@@ -16,3 +16,17 @@ function divide(a, b) {
     return a / b;
 }
 
+let num1, operator, num2;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
