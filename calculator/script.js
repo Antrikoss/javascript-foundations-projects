bb9f559d23e50257e63dd7cd28d11e26a817cacf
Andreas <andrewtheodoses@gmail.com>
2026-08-19 22:21:22 +0300

    Make buttons dynamically update variables and display

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,6 @@
+let num1 = operator = num2 = null;
+const display = document.querySelector('.display');
+const digits = document.querySelectorAll('.number');
 
 // Basic functions
 function add(a, b) {
@@ -16,17 +19,74 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1, operator, num2;
-
-function operate(operator, num1, num2) {
+function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            return add(num1, num2);
+            return add(a, b);
         case '-':
-            return subtract(num1, num2);
+            return subtract(a, b);
         case '*':
-            return multiply(num1, num2);
+            return multiply(a, b);
         case '/':
-            return divide(num1, num2);
+            return divide(a, b);
     }
 }
+
+function updateNum1(n) {
+    num1 = n;
+}
+
+function updateNum2(n) {
+    num2 = n;
+}
+
+function updateDisplay(n) {
+    if (!Number.isInteger(n))
+        display.innerText = parseFloat(n.toFixed(9));
+    else 
+        display.innerText = n;
+}
+
+updateDisplay(14);
+
+digits.forEach(digit => {
+    digit.addEventListener('click', () => {
+        let num ;
+        switch(digit.classList[1]) {
+            case 'zero':
+                num = 0;
+                break;
+            case 'one':
+                num = 1;
+                break;
+            case 'two':
+                num = 2;
+                break;
+            case 'three':
+                num = 3;
+                break;
+            case 'four':
+                num = 4;
+                break;
+            case 'five':
+                num = 5;
+                break;
+            case 'six':
+                num = 6;
+                break;
+            case 'seven':
+                num = 7;
+                break;
+            case 'eight':
+                num = 8;
+                break;
+            case 'nine':
+                num = 9;
+                break;
+        }
+        if (num1 === null)
+            updateNum1(num);
+        updateDisplay(num1);
+        num1 = null;
+    });
+});
